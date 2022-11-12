782fbc421823c4b85cefc922ec6de40df8c95fc0
Erdum <erdumadnan@gmail.com>
2022-11-12 06:27:02 +0000

    TCP Server created

---
+++ server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"net"
+)
+
+func server() {
+	ln, err := net.Listen("tcp", ":9999")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		
+		go handleServerConnection(c)
+	}
+}
+
+func handleServerConnection(c net.Conn) {
+	var msg string
+	err := gob.NewDecoder()c.Decode(&msg)
+	if err != nill {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Received", msg)
+	}
+	
+	c.Close()
+}
