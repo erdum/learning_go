c9c85ba5d6fbdaa6ba8b0696a5ab8ff203eb80d9
Erdum <erdumadnan@gmail.com>
2022-11-12 16:01:28 +0500

    Update feedback added

--- server.go
+++ server.go
@@ -8,25 +8,35 @@ import (
 
 func server(ln net.Listener) {
 	for {
+		fmt.Println("Listening for requests...")
 		c, err := ln.Accept()
+		fmt.Println("Connectection accepted")
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 
-		var msg string
-		err = gob.NewDecoder(c).Decode(&msg)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println("Received", msg)
+		for {
+		  var msg string
+		  err = gob.NewDecoder(c).Decode(&msg)
+		  if msg == "close" {
+			  c.Close()
+			  fmt.Println("Connectection closed")
+			  break
+		  }
+		  if err != nil {
+			  fmt.Println(err)
+		  } else {
+			  fmt.Println("Received", msg)
+		  }
+
+		  fmt.Println("33")
 		}
-		c.Close()
 	}
 }
 
 func main() {
-	ln, err := net.Listen("tcp", "127.0.0.1:9999")
+	ln, err := net.Listen("tcp", "0.0.0.0:9999")
 	if err != nil {
 		fmt.Println(err)
 		return
