93d1c8f3c19657aabca8df7d1c3ec4a81ceb6628
Erdum <erdumadnan@gmail.com>
2022-11-12 06:59:17 +0000

    Client added

--- server.go
+++ server.go
@@ -35,8 +35,26 @@ func handleServerConnection(c net.Conn) {
 	c.Close()
 }
 
+func client() {
+  c, err := net.Dial("tcp", "127.0.0.1:9999")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  msg := "Hello World"
+  fmt.Println("Sending", msg)
+  err = gob.NewEncoder(c).Encode(msg)
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  c.Close()
+}
+
 func main() {
 	go server()
+	go client()
 
 	var input string
 	fmt.Scanln(&input)
